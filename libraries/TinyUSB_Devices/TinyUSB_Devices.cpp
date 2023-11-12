dbe06d46a63e15d98005e6128e9fea68c536ba7c
That One Seong <7321839+SeongGino@users.noreply.github.com>
2023-11-12 20:00:30 +0000

    Fixed up new input library

---
+++ libraries/TinyUSB_Devices/TinyUSB_Devices.cpp
@@ -0,0 +1,411 @@
+/*
+ * This module (hastily) combines Mike Lynch's modified "AbsMouse5" library, and
+ * Chris Young's TinyUSB Mouse and Keyboard library (the Keyboard half, anyways),
+ * which in itself uses pieces of Arduino's basic Keyboard library.
+ */
+#if defined(USE_TINYUSB)
+  #include <Adafruit_TinyUSB.h>
+#elif defined(CFG_TUSB_MCU)
+  #error Incompatible USB stack. Use Adafruit TinyUSB.
+#else
+  #include <HID.h>
+#endif
+
+#include "TinyUSB_Devices.h"
+
+// USB HID Report ID
+enum HID_RID_e{
+  HID_RID_KEYBOARD = 1,
+  HID_RID_MOUSE
+};
+
+// HID report descriptor using TinyUSB's template
+uint8_t const desc_hid_report[] = {
+  TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(HID_RID_KEYBOARD)),
+  TUD_HID_REPORT_DESC_ABSMOUSE5(HID_REPORT_ID(HID_RID_MOUSE))
+};
+
+int __USBGetKeyboardReportID()
+{
+    return HID_RID_KEYBOARD;
+}
+  
+/*****************************
+ *   MOUSE SECTION
+ *****************************/ 
+
+#if defined(_USING_HID)
+static const uint8_t HID_REPORT_DESCRIPTOR5[] PROGMEM = {
+	0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+	0x09, 0x02,        // Usage (Mouse)
+	0xA1, 0x01,        // Collection (Application)
+	0x09, 0x01,        //   Usage (Pointer)
+	0xA1, 0x00,        //   Collection (Physical)
+	0x85, 0x01,        //     Report ID (1)
+	0x05, 0x09,        //     Usage Page (Button)
+	0x19, 0x01,        //     Usage Minimum (0x01)
+	0x29, 0x05,        //     Usage Maximum (0x05)
+	0x15, 0x00,        //     Logical Minimum (0)
+	0x25, 0x01,        //     Logical Maximum (1)
+	0x95, 0x05,        //     Report Count (5)
+	0x75, 0x01,        //     Report Size (1)
+	0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+	0x95, 0x01,        //     Report Count (1)
+	0x75, 0x03,        //     Report Size (3)
+	0x81, 0x03,        //     Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+	0x05, 0x01,        //     Usage Page (Generic Desktop Ctrls)
+	0x09, 0x30,        //     Usage (X)
+	0x09, 0x31,        //     Usage (Y)
+	0x16, 0x00, 0x00,  //     Logical Minimum (0)
+	0x26, 0xFF, 0x7F,  //     Logical Maximum (32767)
+	0x36, 0x00, 0x00,  //     Physical Minimum (0)
+	0x46, 0xFF, 0x7F,  //     Physical Maximum (32767)
+	0x75, 0x10,        //     Report Size (16)
+	0x95, 0x02,        //     Report Count (2)
+	0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+	0xC0,              //   End Collection
+	0xC0               // End Collection
+};
+#endif // _USING_HID
+
+AbsMouse5_::AbsMouse5_(uint8_t reportId) : _reportId(reportId), _buttons(0), _x(0), _y(0), _width(32767l), _height(32767l), _autoReport(true)
+{
+#if defined(_USING_HID)
+	static HIDSubDescriptor descriptorNode(HID_REPORT_DESCRIPTOR5, sizeof(HID_REPORT_DESCRIPTOR5));
+	HID().AppendDescriptor(&descriptorNode);
+#endif // _USING_HID
+}
+
+void AbsMouse5_::init(uint16_t width, uint16_t height, bool autoReport)
+{
+	_width = width;
+	_height = height;
+	_autoReport = autoReport;
+}
+
+void AbsMouse5_::report(void)
+{
+	uint8_t buffer[5];
+	buffer[0] = _buttons;
+	buffer[1] = _x & 0xFF;
+	buffer[2] = (_x >> 8) & 0xFF;
+	buffer[3] = _y & 0xFF;
+	buffer[4] = (_y >> 8) & 0xFF;
+#if defined(_USING_HID)
+	HID().SendReport(_reportId, buffer, 5);
+#endif // _USING_HID
+#if defined(USE_TINYUSB)
+	tud_hid_report(_reportId, buffer, 5);
+#endif // USE_TINYUSB
+}
+
+void AbsMouse5_::move(uint16_t x, uint16_t y)
+{
+	x = (uint16_t)((32767l * ((uint32_t)x)) / _width);
+	y = (uint16_t)((32767l * ((uint32_t)y)) / _height);
+
+	if(x != _x || y != _y) {
+		_x = x;
+		_y = y;
+		if(_autoReport) {
+			report();
+		}
+	}
+}
+
+void AbsMouse5_::press(uint8_t button)
+{
+	_buttons |= button;
+
+	if(_autoReport) {
+		report();
+	}
+}
+
+void AbsMouse5_::release(uint8_t button)
+{
+	_buttons &= ~button;
+
+	if(_autoReport) {
+		report();
+	}
+}
+  
+ /*****************************
+ *   KEYBOARD SECTION
+ *****************************/ 
+
+Adafruit_USBD_HID usbHid;
+
+  Keyboard_::Keyboard_(void) {
+  }
+  
+  void Keyboard_::begin(void)
+  {
+    usbHid.setPollInterval(1);
+    usbHid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
+    //usb_hid.setStringDescriptor("TinyUSB HID Composite");
+
+    usbHid.begin();
+  }
+  
+  void Keyboard_::end(void)
+  {
+  }
+  
+  void Keyboard_::sendReport(KeyReport* keys)
+  {
+    if ( USBDevice.suspended() )  {
+      USBDevice.remoteWakeup();
+    }
+    while(!usbHid.ready()) delay(1);
+    usbHid.keyboardReport(__USBGetKeyboardReportID(),keys->modifiers,keys->keys);
+    delay(2);
+  }
+  
+  #define SHIFT 0x80
+  const uint8_t _asciimap[128] =
+  {
+    0x00,             // NUL
+    0x00,             // SOH
+    0x00,             // STX
+    0x00,             // ETX
+    0x00,             // EOT
+    0x00,             // ENQ
+    0x00,             // ACK  
+    0x00,             // BEL
+    0x2a,     // BS Backspace
+    0x2b,     // TAB  Tab
+    0x28,     // LF Enter
+    0x00,             // VT 
+    0x00,             // FF 
+    0x00,             // CR 
+    0x00,             // SO 
+    0x00,             // SI 
+    0x00,             // DEL
+    0x00,             // DC1
+    0x00,             // DC2
+    0x00,             // DC3
+    0x00,             // DC4
+    0x00,             // NAK
+    0x00,             // SYN
+    0x00,             // ETB
+    0x00,             // CAN
+    0x00,             // EM 
+    0x00,             // SUB
+    0x00,             // ESC
+    0x00,             // FS 
+    0x00,             // GS 
+    0x00,             // RS 
+    0x00,             // US 
+  
+    0x2c,      //  ' '
+    0x1e|SHIFT,    // !
+    0x34|SHIFT,    // "
+    0x20|SHIFT,    // #
+    0x21|SHIFT,    // $
+    0x22|SHIFT,    // %
+    0x24|SHIFT,    // &
+    0x34,          // '
+    0x26|SHIFT,    // (
+    0x27|SHIFT,    // )
+    0x25|SHIFT,    // *
+    0x2e|SHIFT,    // +
+    0x36,          // ,
+    0x2d,          // -
+    0x37,          // .
+    0x38,          // /
+    0x27,          // 0
+    0x1e,          // 1
+    0x1f,          // 2
+    0x20,          // 3
+    0x21,          // 4
+    0x22,          // 5
+    0x23,          // 6
+    0x24,          // 7
+    0x25,          // 8
+    0x26,          // 9
+    0x33|SHIFT,      // :
+    0x33,          // ;
+    0x36|SHIFT,      // <
+    0x2e,          // =
+    0x37|SHIFT,      // >
+    0x38|SHIFT,      // ?
+    0x1f|SHIFT,      // @
+    0x04|SHIFT,      // A
+    0x05|SHIFT,      // B
+    0x06|SHIFT,      // C
+    0x07|SHIFT,      // D
+    0x08|SHIFT,      // E
+    0x09|SHIFT,      // F
+    0x0a|SHIFT,      // G
+    0x0b|SHIFT,      // H
+    0x0c|SHIFT,      // I
+    0x0d|SHIFT,      // J
+    0x0e|SHIFT,      // K
+    0x0f|SHIFT,      // L
+    0x10|SHIFT,      // M
+    0x11|SHIFT,      // N
+    0x12|SHIFT,      // O
+    0x13|SHIFT,      // P
+    0x14|SHIFT,      // Q
+    0x15|SHIFT,      // R
+    0x16|SHIFT,      // S
+    0x17|SHIFT,      // T
+    0x18|SHIFT,      // U
+    0x19|SHIFT,      // V
+    0x1a|SHIFT,      // W
+    0x1b|SHIFT,      // X
+    0x1c|SHIFT,      // Y
+    0x1d|SHIFT,      // Z
+    0x2f,          // [
+    0x31,          // bslash
+    0x30,          // ]
+    0x23|SHIFT,    // ^
+    0x2d|SHIFT,    // _
+    0x35,          // `
+    0x04,          // a
+    0x05,          // b
+    0x06,          // c
+    0x07,          // d
+    0x08,          // e
+    0x09,          // f
+    0x0a,          // g
+    0x0b,          // h
+    0x0c,          // i
+    0x0d,          // j
+    0x0e,          // k
+    0x0f,          // l
+    0x10,          // m
+    0x11,          // n
+    0x12,          // o
+    0x13,          // p
+    0x14,          // q
+    0x15,          // r
+    0x16,          // s
+    0x17,          // t
+    0x18,          // u
+    0x19,          // v
+    0x1a,          // w
+    0x1b,          // x
+    0x1c,          // y
+    0x1d,          // z
+    0x2f|SHIFT,    // {
+    0x31|SHIFT,    // |
+    0x30|SHIFT,    // }
+    0x35|SHIFT,    // ~
+    0       // DEL
+  };
+  
+  // press() adds the specified key (printing, non-printing, or modifier)
+  // to the persistent key report and sends the report.  Because of the way 
+  // USB HID works, the host acts like the key remains pressed until we 
+  // call release(), releaseAll(), or otherwise clear the report and resend.
+  size_t Keyboard_::press(uint8_t k)
+  {
+    uint8_t i;
+    if (k >= 136) {     // it's a non-printing key (not a modifier)
+      k = k - 136;
+    } else if (k >= 128) {  // it's a modifier key
+      _keyReport.modifiers |= (1<<(k-128));
+      k = 0;
+    } else {        // it's a printing key
+      k = pgm_read_byte(_asciimap + k);
+      if (!k) {
+        setWriteError();
+        return 0;
+      }
+      if (k & 0x80) {           // it's a capital letter or other character reached with shift
+        _keyReport.modifiers |= 0x02; // the left shift modifier
+        k &= 0x7F;
+      }
+    }
+    
+    // Add k to the key report only if it's not already present
+    // and if there is an empty slot.
+    if (_keyReport.keys[0] != k && _keyReport.keys[1] != k && 
+      _keyReport.keys[2] != k && _keyReport.keys[3] != k &&
+      _keyReport.keys[4] != k && _keyReport.keys[5] != k) {
+      
+      for (i=0; i<6; i++) {
+        if (_keyReport.keys[i] == 0x00) {
+          _keyReport.keys[i] = k;
+          break;
+        }
+      }
+      if (i == 6) {
+        setWriteError();
+        return 0;
+      } 
+    }
+    sendReport(&_keyReport);
+    return 1;
+  }
+  
+  // release() takes the specified key out of the persistent key report and
+  // sends the report.  This tells the OS the key is no longer pressed and that
+  // it shouldn't be repeated any more.
+  size_t Keyboard_::release(uint8_t k)
+  {
+    uint8_t i;
+    if (k >= 136) {     // it's a non-printing key (not a modifier)
+      k = k - 136;
+    } else if (k >= 128) {  // it's a modifier key
+      _keyReport.modifiers &= ~(1<<(k-128));
+      k = 0;
+    } else {        // it's a printing key
+      k = pgm_read_byte(_asciimap + k);
+      if (!k) {
+        return 0;
+      }
+      if (k & 0x80) {             // it's a capital letter or other character reached with shift
+        _keyReport.modifiers &= ~(0x02);  // the left shift modifier
+        k &= 0x7F;
+      }
+    }
+    
+    // Test the key report to see if k is present.  Clear it if it exists.
+    // Check all positions in case the key is present more than once (which it shouldn't be)
+    for (i=0; i<6; i++) {
+      if (0 != k && _keyReport.keys[i] == k) {
+        _keyReport.keys[i] = 0x00;
+      }
+    }
+    sendReport(&_keyReport);
+    return 1;
+  }
+  
+  void Keyboard_::releaseAll(void)
+  {
+    _keyReport.keys[0] = 0;
+    _keyReport.keys[1] = 0; 
+    _keyReport.keys[2] = 0;
+    _keyReport.keys[3] = 0; 
+    _keyReport.keys[4] = 0;
+    _keyReport.keys[5] = 0; 
+    _keyReport.modifiers = 0;
+    sendReport(&_keyReport);
+  }
+  
+  size_t Keyboard_::write(uint8_t c)
+  {
+    uint8_t p = press(c);  // Keydown
+    release(c);            // Keyup
+    return p;              // just return the result of press() since release() almost always returns 1
+  }
+  
+  size_t Keyboard_::write(const uint8_t *buffer, size_t size) {
+    size_t n = 0;
+    while (size--) {
+      if (*buffer != '\r') {
+        if (write(*buffer)) {
+          n++;
+        } else {
+          break;
+        }
+      }
+      buffer++;
+    }
+    return n;
+  }
+  Keyboard_ Keyboard;//create an instance of the Keyboard object
---
+++ libraries/TinyUSB_Devices/TinyUSB_Devices.h
@@ -0,0 +1,202 @@
+/*
+ * This module simulates the standard Arduino "Mouse.h" and
+ * "Keyboard.h" API for use with the TinyUSB HID API. Instead of doing
+ *  #include <HID.h>
+ *  #include <Mouse.h>
+ *  #include <Keyboard.h>
+ *  
+ *  Simply do
+ *  
+ *  #include <TinyUSB_Mouse_Keyboard.h>
+ *  
+ *  and this module will automatically select whether or not to use the
+ *  standard Arduino mouse and keyboard API or the TinyUSB API. We had to
+ *  combine them into a single library because of the way TinyUSB handles
+ *  descriptors.
+ *  
+ *  For details on Arduino Mouse.h see
+ *   https://www.arduino.cc/reference/en/language/functions/usb/mouse/
+ *  For details on Arduino Keyboard.h see
+ *   https://www.arduino.cc/reference/en/language/functions/usb/keyboard/
+ *
+ *  NOTE: This code is derived from the standard Arduino Mouse.h, Mouse.cpp,
+ *    Keyboard.h, and Keyboard.cpp code. The copyright on that original code
+ *    is as follows.
+ *   
+ *  Copyright (c) 2015, Arduino LLC
+ *  Original code (pre-library): Copyright (c) 2011, Peter Barrett
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <Arduino.h>
+
+/*****************************
+ *   MOUSE SECTION
+ *****************************/ 
+#ifndef _ABSMOUSE5_H_
+#define _ABSMOUSE5_H_
+
+#include <stdint.h>
+
+#define MOUSE_LEFT 0x01
+#define MOUSE_RIGHT 0x02
+#define MOUSE_MIDDLE 0x04
+#define MOUSE_BUTTON4 0x08
+#define MOUSE_BUTTON5 0x10
+
+#if defined(USE_TINYUSB)
+#define TUD_HID_REPORT_DESC_ABSMOUSE5(...) \
+	0x05, 0x01, \
+	0x09, 0x02, \
+	0xA1, 0x01, \
+	__VA_ARGS__ \
+	0x09, 0x01, \
+	0xA1, 0x00, \
+	0x05, 0x09, \
+	0x19, 0x01, \
+	0x29, 0x05, \
+	0x15, 0x00, \
+	0x25, 0x01, \
+	0x95, 0x05, \
+	0x75, 0x01, \
+	0x81, 0x02, \
+	0x95, 0x01, \
+	0x75, 0x03, \
+	0x81, 0x03, \
+	0x05, 0x01, \
+	0x09, 0x30, \
+	0x09, 0x31, \
+	0x16, 0x00, 0x00, \
+	0x26, 0xFF, 0x7F, \
+	0x36, 0x00, 0x00, \
+	0x46, 0xFF, 0x7F, \
+	0x75, 0x10, \
+	0x95, 0x02, \
+	0x81, 0x02, \
+	0xC0, \
+	0xC0
+#endif // USE_TINYUSB
+
+// 5 button absolute mouse
+class AbsMouse5_
+{
+private:
+	const uint8_t _reportId;
+	uint8_t _buttons;
+	uint16_t _x;
+	uint16_t _y;
+	uint32_t _width;
+	uint32_t _height;
+	bool _autoReport;
+
+public:
+	AbsMouse5_(uint8_t reportId = 1);
+	void init(uint16_t width = 32767, uint16_t height = 32767, bool autoReport = true);
+	void report(void);
+	void move(uint16_t x, uint16_t y);
+	void press(uint8_t b = MOUSE_LEFT);
+	void release(uint8_t b = MOUSE_LEFT);
+};
+
+// global singleton
+extern AbsMouse5_ AbsMouse5;
+
+#endif // _ABSMOUSE5_H_
+
+/******************************
+ *    KEYBOARD SECTION
+ ******************************/
+  //  Keyboard codes
+  //  Note these are different in some respects to the TinyUSB codes but 
+  //  are compatible with Arduino Keyboard.h API
+  
+  #define KEY_LEFT_CTRL   0x80
+  #define KEY_LEFT_SHIFT    0x81
+  #define KEY_LEFT_ALT    0x82
+  #define KEY_LEFT_GUI    0x83
+  #define KEY_RIGHT_CTRL    0x84
+  #define KEY_RIGHT_SHIFT   0x85
+  #define KEY_RIGHT_ALT   0x86
+  #define KEY_RIGHT_GUI   0x87
+  
+  #define KEY_UP_ARROW    0xDA
+  #define KEY_DOWN_ARROW    0xD9
+  #define KEY_LEFT_ARROW    0xD8
+  #define KEY_RIGHT_ARROW   0xD7
+  #define KEY_BACKSPACE   0xB2
+  #define KEY_TAB       0xB3
+  #define KEY_RETURN      0xB0
+  #define KEY_ESC       0xB1
+  #define KEY_INSERT      0xD1
+  #define KEY_DELETE      0xD4
+  #define KEY_PAGE_UP     0xD3
+  #define KEY_PAGE_DOWN   0xD6
+  #define KEY_HOME      0xD2
+  #define KEY_END       0xD5
+  #define KEY_CAPS_LOCK   0xC1
+  #define KEY_F1        0xC2
+  #define KEY_F2        0xC3
+  #define KEY_F3        0xC4
+  #define KEY_F4        0xC5
+  #define KEY_F5        0xC6
+  #define KEY_F6        0xC7
+  #define KEY_F7        0xC8
+  #define KEY_F8        0xC9
+  #define KEY_F9        0xCA
+  #define KEY_F10       0xCB
+  #define KEY_F11       0xCC
+  #define KEY_F12       0xCD
+  #define KEY_F13       0xF0
+  #define KEY_F14       0xF1
+  #define KEY_F15       0xF2
+  #define KEY_F16       0xF3
+  #define KEY_F17       0xF4
+  #define KEY_F18       0xF5
+  #define KEY_F19       0xF6
+  #define KEY_F20       0xF7
+  #define KEY_F21       0xF8
+  #define KEY_F22       0xF9
+  #define KEY_F23       0xFA
+  #define KEY_F24       0xFB
+  
+  //  Low level key report: up to 6 keys and shift, ctrl etc at once
+  typedef struct
+  {
+    uint8_t modifiers;
+    uint8_t reserved;
+    uint8_t keys[6];
+  } KeyReport;
+  
+  /*
+   * This class contains the exact same methods as the Arduino Keyboard.h class.
+   */
+  
+  class Keyboard_ : public Print
+  {
+  private:
+    KeyReport _keyReport;
+    void sendReport(KeyReport* keys);
+  public:
+    Keyboard_(void);
+    void begin(void);
+    void end(void);
+    size_t write(uint8_t k);
+    size_t write(const uint8_t *buffer, size_t size);
+    size_t press(uint8_t k);
+    size_t release(uint8_t k);
+    void releaseAll(void);
+  };
+extern Keyboard_ Keyboard;
