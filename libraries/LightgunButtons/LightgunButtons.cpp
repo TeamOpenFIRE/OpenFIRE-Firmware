7de38047373cda62de9e609e59aabd72c2461d01
SeongGino <marco98gonzalez@gmail.com>
2025-04-10 16:26:10 -0400

    Oops 2: the oopsening

--- libraries/LightgunButtons/LightgunButtons.cpp
+++ libraries/LightgunButtons/LightgunButtons.cpp
@@ -171,7 +171,7 @@ uint32_t LightgunButtons::Poll(unsigned long minTicks)
                         // state is low, button is pressed
 
                         // if reporting is enabled for the button (no trigger reporting)
-                        if(i != 1 && report & bitMask) {
+                        if(i && report & bitMask) {
                             reportedPressed |= bitMask;
                             if(analogOutput) {
                                 if(btn.reportType3 == ReportType_Mouse) {
@@ -230,7 +230,7 @@ uint32_t LightgunButtons::Poll(unsigned long minTicks)
                         // if the button press was reported then report the release
                         // note that the report flag is ignored here to avoid stuck buttons
                         // in case the reporting is disabled while button(s) are pressed
-                        if(i != 1 && reportedPressed & bitMask) {
+                        if(i && reportedPressed & bitMask) {
                             reportedPressed &= ~bitMask;
                             if(analogOutput) {
                                 if(btn.reportType3 == ReportType_Mouse) {
