be0c6a585c8cb8e49d5a2e46862a2e61731ccf8d
SeongGino <marco98gonzalez@gmail.com>
2025-04-10 16:06:23 -0400

    Consistency fix, also inhibit releasing for index 0 too

--- libraries/LightgunButtons/LightgunButtons.cpp
+++ libraries/LightgunButtons/LightgunButtons.cpp
@@ -230,7 +230,7 @@ uint32_t LightgunButtons::Poll(unsigned long minTicks)
                         // if the button press was reported then report the release
                         // note that the report flag is ignored here to avoid stuck buttons
                         // in case the reporting is disabled while button(s) are pressed
-                        if(reportedPressed & bitMask) {
+                        if(i != 1 && reportedPressed & bitMask) {
                             reportedPressed &= ~bitMask;
                             if(analogOutput) {
                                 if(btn.reportType3 == ReportType_Mouse) {
